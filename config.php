245fc6f66dec817d2449c388625a15d6aab24b48
hitsme <1182681022@qq.com>
2018-10-19 22:10:59 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('APIKEY', 'AIzaSyB5zY2fq6YRSMxF7HwGb8rEQ8VVRxiO6ps');
 define('GJ_CODE', 'HK');
-define('SITE_NAME', 'you2beclub');
+define('SITE_NAME', 'you2b1eclub');
 define('TITLENAME', 'you2beclub');
 define('EN2DEKEY', '32123wwerwertgfdgds');
 define('EMAIL', '1334347212@qq.com');
@@ -17,12 +17,12 @@ define('LOGINHTML', '<!DOCTYPE html>
 <body>
 <center>
     <p id="login">
-    <h1>µÇÂ½</h1>
+    <h1>ÂµÃ‡Ã‚Â½</h1>
     <form method="post" style="margin:0px auto;">
-        <input type="text" required="required" placeholder="ÓÃ»§Ãû" name="name"></input>
+        <input type="text" required="required" placeholder="Ã“ÃƒÂ»Â§ÃƒÃ»" name="name"></input>
         <br>
-        <input type="password" required="required" placeholder="ÃÜÂë" name="password"></input>
-        <button class="but" type="submit">µÇÂ¼</button>
+        <input type="password" required="required" placeholder="ÃƒÃœÃ‚Ã«" name="password"></input>
+        <button class="but" type="submit">ÂµÃ‡Ã‚Â¼</button>
     </form>
     </p>
 </center>
